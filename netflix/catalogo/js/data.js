4e67c062c4f4b506c3e73ae6fb3f788889441cfc
Iris Bispo <141687554+Irisabispo@users.noreply.github.com>
2026-03-30 14:56:47 -0400

    p

---
+++ netflix/catalogo/js/data.js
@@ -0,0 +1,42 @@
+export const categories = [
+    {
+        title: "Mais Assistidos",
+        items: [
+            {
+                img: "https://tse2.mm.bing.net/th/id/OIP.0TdPEuZtIxWcG7a1YJp5JQHaEK?rs=1&pid=ImgDetMain&o=7&rm=3", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://youtu.be/A1Sk9sJz2JQ"
+            },
+            {
+                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
+            },
+            {
+                img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
+            },
+            {
+                img: "https://th.bing.com/th/id/OIP.BNEW6gBcDbOlN-Lfnsg83gHaEK?w=291&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3", progress: 0,
+                youtube: "https://youtu.be/SLHypJkD2OY"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://tse2.mm.bing.net/th/id/OIP.ah7lQDNC6EPS2aHH6aYXxAHaEM?w=900&h=510&rs=1&pid=ImgDetMain&o=7&rm=3", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://th.bing.com/th/id/OIP.dRuPtnhWtnyEsCAy2_tUlgHaEK?w=322&h=181&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://tse2.mm.bing.net/th/id/OIP.2VYkgUBQeCPxfpBZsSjnvAHaEK?rs=1&pid=ImgDetMain&o=7&rm=3", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://th.bing.com/th/id/OIP.Js9dnWXMRjEwbc694vZxmgHaDH?w=326&h=147&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    },
+    {
+        title: "Para Maratonar",
+        items: [
+            { img: "https://th.bing.com/th/id/OIP.VNQBo0_L53DyVelcGMO2tQHaEJ?w=281&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3", top10: true, youtube: "https://youtu.be/Apn96-Jk-yE" },
+            { img: "https://th.bing.com/th/id/OIP.PuDS12xDB00M6u2yYERSLQHaEK?w=261&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://youtu.be/31A0DqX6jHI" },
+            { img: "https://th.bing.com/th/id/OIP.bvpF2bDhZlzSIDFtGtxRrgHaEK?w=292&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://youtu.be/7kE1eds0VMA" },
+            { img: "https://th.bing.com/th?id=OIF.WZdK67Ac%2biND%2b%2fcW4dndnQ&w=305&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://youtu.be/hfu7vf8kvjA" },
+        ]
+    }
+];
+
